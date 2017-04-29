6d371c1a2fd0426763f7bf0e4009d4e246660be9
Jacques <wpbxwpbx@gmail.com>
2017-04-29 00:25:20 -0400

    Bug fix

--- MusicLibrary/MainWindow.xaml.cs
+++ MusicLibrary/MainWindow.xaml.cs
@@ -1,14 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing.Printing;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
